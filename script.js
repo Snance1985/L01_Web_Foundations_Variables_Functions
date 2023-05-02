93c8c82421f4968d52ebf8dff253caa3bf677898
Sean Nance <sean_nance@hotmail.com>
2023-05-01 19:54:36 -0700

    modified script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function stringCounter(sentence) {
+    var reslt = sentence.split(" ");
+    console.log (reslt.length);
+    var length = sentence.length;
+    console.log(length)
+}
+
+let sentence2 = "A sunset on Mars is blue";
+stringCounter(sentence2);
+
