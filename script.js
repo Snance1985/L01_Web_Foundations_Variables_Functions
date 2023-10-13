b914bf55a40953474e8f8124b8f9e826081b1db4
Sean Nance <sean_nance@hotmail.com>
2023-10-13 08:29:48 -0700

    created index.html and script.js files

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function stringCounter(sentence) {
     var reslt = sentence.split(" ");
     console.log (reslt.length);
     var length = sentence.length;
-    console.log(length)
+    console.log(length);
 }
 
 let sentence2 = "A sunset on Mars is blue";
